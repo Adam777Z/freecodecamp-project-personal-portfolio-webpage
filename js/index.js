8e85071bc1d7b8649804ff768178d4dcc7c6763b
Adam777 <Adam777-5867@outlook.com>
2018-12-28 19:16:15 +0000

    Other Project screenshots added and other changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 const projectName = 'portfolio';
 localStorage.setItem('example_project', 'Personal Portfolio');
 
-$('body').scrollspy({ target: '#navbar' });
+$('body').scrollspy({
+	target: '#navbar'
+});
+
+$('a[href*="#"]').on('click', function(e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top
+	}, 200, 'linear');
+});
