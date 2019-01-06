e9ce27843434a038e2d3fa7786205b918edf5a67
Adam777 <Adam777-5867@outlook.com>
2019-01-06 16:00:57 +0000

    Fix

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 const projectName = 'portfolio';
 localStorage.setItem('example_project', 'Personal Portfolio');
 
-$('body').scrollspy({
-	target: '#navbar'
+$(document).ready(function() {
+	$('body').scrollspy({
+		target: '#navbar'
+	});
+
+	// Smooth Scroll to ID (disabled for the test to pass)
+	// $('a[href*="#"]').on('click', function(e) {
+	// 	e.preventDefault();
+
+	// 	$('html, body').animate({
+	// 		scrollTop: $($(this).attr('href')).offset().top
+	// 	}, 200, 'linear');
+	// });
 });
-
-// Smooth Scroll to ID (disabled for the test to pass)
-// $('a[href*="#"]').on('click', function(e) {
-// 	e.preventDefault();
-
-// 	$('html, body').animate({
-// 		scrollTop: $($(this).attr('href')).offset().top
-// 	}, 200, 'linear');
-// });
