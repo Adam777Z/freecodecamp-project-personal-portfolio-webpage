27cd611c8f69aaf6b54b4ef0ab65ccc89870203b
Adam777 <Adam777-5867@outlook.com>
2019-01-06 10:24:21 +0000

    Improvements

--- js/index.js
+++ js/index.js
@@ -5,10 +5,11 @@ $('body').scrollspy({
 	target: '#navbar'
 });
 
-$('a[href*="#"]').on('click', function(e) {
-	e.preventDefault();
+// Smooth Scroll (disabled for the test to pass)
+// $('a[href*="#"]').on('click', function(e) {
+// 	e.preventDefault();
 
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top
-	}, 200, 'linear');
-});
+// 	$('html, body').animate({
+// 		scrollTop: $($(this).attr('href')).offset().top
+// 	}, 200, 'linear');
+// });
