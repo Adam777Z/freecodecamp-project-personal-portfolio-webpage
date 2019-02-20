046d6a495a6319148d2b01bbbbd9570df5daf403
Adam777 <Adam777-5867@outlook.com>
2019-02-20 12:24:07 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -31,32 +31,29 @@ function onloadCallback() {
 }
 
 function onSubmit(token) {
-	$('#submit-button').attr('disabled', 'disabled');
+	$('#submit-button').attr('disabled', 'disabled').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> <span>Sending...</span>');
 	$('#contact-form-result .alert').alert('close');
-	$('#spinner').show();
 
 	$.ajax({
-			url: 'https://usebasin.com/f/f8a55f3aacfc.json',
-			method: 'POST',
-			data: {
-				'Name': $('#name').val(),
-				'Email': $('#email').val(),
-				'Message': $('#message').val(),
-				'Source': 'CodePen',
-				'g-recaptcha-response': token
-			},
-			dataType: 'json'
-		})
-		.done(function() {
-			$('#spinner').hide();
-			$('#submit-button').removeAttr('disabled');
-			$('#contact-form-result').html('<div class="alert alert-success alert-dismissible fade show mt-2 mb-0" role="alert"><span>Email sent successfully.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-			// $('#name, #email, #message').val('');
-			$('#contact-form').trigger('reset');
-		})
-		.fail(function() {
-			$('#spinner').hide();
-			$('#submit-button').removeAttr('disabled');
-			$('#contact-form-result').html('<div class="alert alert-danger alert-dismissible fade show mt-2 mb-0" role="alert"><span>Error. Please try again.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-		});
+		url: 'https://usebasin.com/f/f8a55f3aacfc.json',
+		method: 'POST',
+		data: {
+			'Name': $('#name').val(),
+			'Email': $('#email').val(),
+			'Message': $('#message').val(),
+			'Source': 'CodePen',
+			'g-recaptcha-response': token
+		},
+		dataType: 'json'
+	})
+	.done(function() {
+		$('#submit-button').removeAttr('disabled').html('Send');
+		$('#contact-form-result').html('<div class="alert alert-success alert-dismissible fade show mt-2 mb-0" role="alert"><span>Email sent successfully.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+		// $('#name, #email, #message').val('');
+		$('#contact-form').trigger('reset');
+	})
+	.fail(function() {
+		$('#submit-button').removeAttr('disabled').html('Send');
+		$('#contact-form-result').html('<div class="alert alert-danger alert-dismissible fade show mt-2 mb-0" role="alert"><span>Error. Please try again.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+	});
 }
